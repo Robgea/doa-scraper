0623e5c9e1a2f1151a5225365a3992a5cb02eb02
Rob <Zhouenkissinger@gmail.com>
2019-06-09 22:24:38 -0400

    Working commit.

---
+++ README.md
@@ -0,0 +1,15 @@
+## Dumbing of Age Scraper
+
+This is a script that scrapes the Dumbing of Age webcomic and downloads every page of it. This renames each comic by the book it's in and by pagenumber. It will also save each comic to a subdirectory for each book that it's in. 
+
+I've done a complete run through and it worked with no real errors (as of June 9th.) This will always download every comic that is available to download as of the time it's run.
+
+This took about three hours to run completely on my computer. If you want to do this in smaller chunks replace the 'url' variable with a different starting point. Changing the end point could be done by futzing with the 'last_url' variable if you really want to do that.
+
+## A note 
+
+If you're here you either really like web scrapers or because you like Dumbing of Age. If that's true, and you have extra income, try supporting the webcomic by directly buying things from them (here are the two store links: https://dumbingofage.bigcartel.com/  https://www.teepublic.com/stores/damnyouwillis).  People like this deserve our respect and support for what they do and there's no better way to show that than materially supporting the work that they do. 
+
+## Future Plans
+
+Most likely not going to touch this again.  If it breaks let me know and I'll see what I can do to make it work.
---
+++ doa_scraper.py
@@ -0,0 +1,80 @@
+from bs4 import BeautifulSoup
+import requests
+import os
+import sys
+import re
+
+
+def dumbing_scraper():
+
+    page_count = 1
+    url = 'http://www.dumbingofage.com/2010/comic/book-1/01-move-in-day/home/'
+    end_condition = False
+    last_com = False
+    book = 'thisgetsreplacedbelow'
+    init_url = requests.get(url)
+    init_soup = BeautifulSoup(init_url.content, "html5lib")
+    last_img = init_soup.find(title = 'Latest')
+    last_url = last_img.get('href')
+
+
+    while end_condition == False:       
+
+        doa_comic = requests.get(url)
+
+
+        if book not in url:
+            book = book_finder(url)
+            os.makedirs(book, exist_ok = True)
+            page_count = 1
+
+        comic_soup = BeautifulSoup(doa_comic.content, "html5lib")
+        imgs = comic_soup.find_all('img',{'src':re.compile('http://www.dumbingofage.com/comics/*')})
+        com_down = imgs[0].get('src')
+
+        image = requests.get(com_down)
+        sys.stdout.write(f'\n Downloading {book} {page_count}')
+        sys.stdout.flush()
+
+        com_name = f'{book} {str(page_count).zfill(3)}{com_down[-4:]}'
+
+        image_file = open(os.path.join(book, os.path.basename(com_name)), 'wb')
+
+        for chunk in image.iter_content(100_000):
+            image_file.write(chunk)
+
+        image_file.close()
+
+
+        if last_com == True:
+            end_condition = True
+            sys.stdout.write('\nAll done!')
+            sys.stdout.flush()
+
+        else:
+
+            next_img = comic_soup.find(title = 'Next')
+            url = next_img.get('href')
+            if url == last_url:
+                last_com = True 
+                sys.stdout.write('\nEnd condition triggered!')
+                sys.stdout.flush()
+            
+            page_count += 1
+
+def book_finder(target):
+    try:
+        book_count = target.find('book-')
+        book_end = book_count + 6
+        book_str = target[book_count:book_end]
+        return book_str
+    except:
+        sys.stdout.write(f'Error happened at {target}')
+        sys.stdout.flush()
+        return 'Errors'
+
+
+
+dumbing_scraper()
+
+
